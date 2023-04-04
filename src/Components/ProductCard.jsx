69c87c9a0846fba5dd3f1a380b6d780ed1ae49bd
unknown <deepanshugulia256@gmail.com>
2023-04-04 12:44:41 +0530

    styling for product cards are done

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import style from './ProductCard.module.css';
 
 const ProductCard = ({ data, addCart }) => {
   return (
-    <div data-testid="product-card">
+    <div data-testid="product-card" className={style.ProductCardContainer}>
       <h3 data-testid="name">Name: {data.name}</h3>
       <h5 data-testid="price">Price: {data.price}</h5>
       <p data-testid="quantity">Quantity: {data.stock} Units</p>
       <button
         data-testid="add-btn"
         onClick={() => {
-          if (data.stock > 0) {
-            return addCart(data);
-          }
+          return addCart(data);
         }}
+        disabled={data.stock ? false : true}
+        className={data.stock ? style.ProductInStock : style.ProductOutStock}
       >
         {data.stock ? 'Add to cart' : 'Out of stock'}
       </button>
